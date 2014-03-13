70346d82470f38105df4b3dee1d3b390038890b0
Ismael Gorissen <ismael.gorissen@gmail.com>
2014-03-13 13:42:44 +0100

    Autoingesttool (Work In Progress)
    + update paths schema JSON file
    + update parameters schema JSON file
    + validate all JSON files

---
+++ libs/index.js
@@ -0,0 +1,65 @@
+var async = require("async");
+var mkdirp = require("mkdirp");
+var jsonschema = require("jsonschema");
+var request = require("request");
+
+var Configurator = require("./configurator");
+var InvalidParametersError = require("../errors/invalid_parameters_error");
+var InvalidPathsError = require("../errors/invalid_paths_error");
+var EmptyFileError = require("../errors/empty_file_error");
+
+var parametersSchema = Configurator.loadSync("parameters_schema");
+var pathsSchema = Configurator.loadSync("paths_schema");
+var Validator = new jsonschema.Validator();
+
+exports.INVALID_PARAMETERS_ERROR = InvalidParametersError;
+exports.INVALID_PATHS_ERROR = InvalidPathsError;
+exports.EMPTY_FILE_ERROR = EmptyFileError;
+exports.downloadReportInPathsWithParameters = downloadReportInPathsWithParameters;
+
+function downloadReportInPathsWithParameters(parameters, paths, callback) {
+    async.waterfall(
+        [
+            function (callback) {
+                validateAllJSON(parameters, paths, callback);
+            }
+        ],
+        function (err, result) {
+            if (err) return callback(err);
+            callback(null, result);
+        }
+    );
+}
+
+function validateAllJSON(parameters, paths, callback) {
+    var parametersErrors = validateJSON(parameters, parametersSchema);
+    var pathsErrors = validateJSON(paths, pathsSchema);
+    var err = null;
+
+    if (parametersErrors) {
+        err = new InvalidParametersError(
+            "Please enter all the required parameters. For help, please download the latest User Guide from the Sales and Trends module in iTunes Connect.",
+            parametersErrors
+        );
+        return callback(err);
+    }
+
+    if (pathsErrors) {
+        err = new InvalidPathsError(
+            "Please enter all the required path parameters.",
+            pathsErrors
+        );
+        return callback(err);
+    }
+
+    callback();
+}
+
+function validateJSON(json, schema) {
+    var result = Validator.validate(json, schema);
+
+    if (result.errors.length > 0)
+        return result.errors;
+
+    return null;
+}
